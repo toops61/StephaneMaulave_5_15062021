38e257900ac340c2ce1dcb985a1bf3ef8409be0d
Toops61 <toops61@gmail.com>
2021-06-15 20:26:17 +0200

    chemins image JS

--- controllers/teddy.js
+++ controllers/teddy.js
@@ -5,7 +5,7 @@ exports.getAllTeddies = (req, res, next) => {
   Teddy.find().then(
     (teddies) => {
       const mappedTeddies = teddies.map((teddy) => {
-        teddy.imageUrl = req.protocol + '://' + req.get('host') + '/public/images/articles/' + teddy.imageUrl;
+        teddy.imageUrl = req.protocol + '://' + req.get('host') + '../public/images/articles/' + teddy.imageUrl;
         return teddy;
       });
       res.status(200).json(mappedTeddies);
@@ -23,7 +23,7 @@ exports.getOneTeddy = (req, res, next) => {
       if (!teddy) {
         return res.status(404).send(new Error('Teddy not found!'));
       }
-      teddy.imageUrl = req.protocol + '://' + req.get('host') + '/public/images/articles/' + teddy.imageUrl;
+      teddy.imageUrl = req.protocol + '://' + req.get('host') + '../public/images/articles/' + teddy.imageUrl;
       res.status(200).json(teddy);
     }
   ).catch(
@@ -64,7 +64,7 @@ exports.orderTeddies = (req, res, next) => {
           if (!teddy) {
             reject('Camera not found: ' + productId);
           }
-          teddy.imageUrl = req.protocol + '://' + req.get('host') + '/public/images/articles/' + teddy.imageUrl;
+          teddy.imageUrl = req.protocol + '://' + req.get('host') + '../public/images/articles/' + teddy.imageUrl;
           resolve(teddy);
         }
       ).catch(
