e837b9caf85606ed1d722d66b3048d1621f61c75
Toops61 <toops61@gmail.com>
2021-06-27 20:50:53 +0200

    JS panier corrections prices

--- BACK/panier.js
+++ BACK/panier.js
@@ -7,15 +7,18 @@ fetch('http://localhost:3000/api/teddies')
     })
     .then(function(value) {
         let totalPanier = 0;
-        let articlesPanier = value;          
+        let articlesPanier = value;   
+        let panierSection = document.getElementById('panier-section');    
         for (let i = 0; i < articlesPanier.length; i++) {
             let produitPrice = value[i].price * 0.01;
-            let panierSection = document.getElementById('panier-section');
-            panierSection.innerHTML += '<div class="panier-resume__article"><div class="panier-resume__article__image" id="article-image' + i + '"></div><div class="panier-resume__article__details"><p class="panier-resume__name">article : Ours en peluche ' + value[i].name + '</p><p class="panier-resume__quantite">quantité : </p><div class="panier-resume__supprimer"><p>supprimer</p></div></div><p class="panier-resume__price" id="produit-price"></p></div>';
+            panierSection.innerHTML += '<div class="panier-resume__article"><div class="panier-resume__article__image" id="article-image' + i + '"></div><div class="panier-resume__article__details"><p class="panier-resume__name">article : Ours en peluche ' + value[i].name + '</p><p class="panier-resume__quantite">quantité : <input type="number" min="1" max"99" value="1" id="quantite-produit"></p><div class="panier-resume__supprimer"><p>supprimer</p></div></div><p class="panier-resume__price" id="produit-price">' + produitPrice + ' €</p></div>';
             document.getElementById("article-image" + i).innerHTML = '<img src="' + value[i].imageUrl + '"alt=ours en peluche"></img>';
+            let produitQuantite = document.getElementById('quantite-produit').value;
+            produitPrice = produitPrice * produitQuantite;
             totalPanier += produitPrice;
         }
-        document.getElementById('total-price').innerHTML += totalPanier;  
+        panierSection.innerHTML += '<p id="total-price">total : </p>';
+        document.getElementById('total-price').innerHTML += totalPanier;
     })
     .catch(function(error) {
         console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
