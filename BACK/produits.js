2662fa2ff7b20e381def2a5156c38c346d2b8c71
Toops61 <toops61@gmail.com>
2021-06-27 17:54:02 +0200

    page index dynamique

--- BACK/produits.js
+++ BACK/produits.js
@@ -5,8 +5,11 @@ fetch('http://localhost:3000/api/teddies')
                 return productsArray;
             }
         })
-        .then(function(value) {
+        .then(function(value) {          
             for (let i = 0; i < value.length; i++) {
+                let productSection = document.getElementById('productsSection');
+                productSection.innerHTML += '<a href="./produit.html"><div class="products__card"><div class="products__card__image" id="teddy-image' + i + '"></div><div class="products__card__description"><h2 id="teddy-name' + i + '"></h2><p id="teddy-price'+i+'"></p></div></div></a>';
+                document.getElementById("teddy-image" + i).innerHTML = '<img src="' + value[i].imageUrl + '" alt=""></img>';
                 document.getElementById("teddy-name" + i).textContent = value[i].name;
                 document.getElementById("teddy-price" + i).textContent = value[i].price*0.01 + ".00 €";
             }   
