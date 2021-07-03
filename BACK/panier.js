d07d750f51f4f7da8075daacebe6e86c572ca3b8
Toops61 <toops61@gmail.com>
2021-07-03 06:50:06 +0200

    adaptab tabindex

--- BACK/panier.js
+++ BACK/panier.js
@@ -24,7 +24,7 @@ for (let i = 0; i < productsId.length; i++) {
         .then(function(produits) {
             let produitPrice = produits[i].prix * 0.01;
             let panierSection = document.getElementById('panier-section');
-            panierSection.innerHTML += '<div class="panier-resume__article"><div class="panier-resume__article__image" id="article-image' + i + '"></div><div class="panier-resume__article__details"><p class="panier-resume__name">article : Ours en peluche ' + produits[i].name + '</p><p class="panier-resume__quantite">quantité : <input type="number" min="1" max="99" value="1" id="quantite-produit"></p><div class="panier-resume__supprimer"><p>supprimer</p></div></div><p class="panier-resume__price" id="produit-price">' + produitPrice + ' €</p></div>';
+            panierSection.innerHTML += '<div class="panier-resume__article"><div class="panier-resume__article__image" tabindex="0" id="article-image' + i + '"></div><div class="panier-resume__article__details"><p tabindex="0" class="panier-resume__name">article : Ours en peluche ' + produits[i].name + '</p><p class="panier-resume__quantite">quantité : <input type="number" min="1" max="99" value="1" id="quantite-produit"></p><div tabindex="0" class="panier-resume__supprimer"><p>supprimer</p></div></div><p tabindex="0" class="panier-resume__price" id="produit-price">' + produitPrice + ' €</p></div>';
             document.getElementById("article-image" + i).innerHTML = '<img src="' + produits[i].imageURL + '"alt=ours en peluche"></img>';
             totalPanier += produitPrice;
             return totalPanier;
--- BACK/produits.js
+++ BACK/produits.js
@@ -8,7 +8,7 @@ fetch('http://localhost:3000/api/teddies')
         .then(function(value) {          
             for (let i = 0; i < value.length; i++) {
                 let productSection = document.getElementById('productsSection');
-                productSection.innerHTML += '<a href="./produit.html?id=' + value[i]._id + '" id="lien-product' + i + '"><div class="products__card"><div class="products__card__image" id="teddy-image' + i + '"></div><div class="products__card__description"><h2 id="teddy-name' + i + '"></h2><p id="teddy-price' + i + '"></p></div></div></a>';
+                productSection.innerHTML += '<a href="./produit.html?id=' + value[i]._id + '" id="lien-product' + i + '"><div class="products__card"><div class="products__card__image" tabindex="0" id="teddy-image' + i + '"></div><div class="products__card__description"><h2 tabindex="0" id="teddy-name' + i + '"></h2><p tabindex="0" id="teddy-price' + i + '"></p></div></div></a>';
                 document.getElementById("teddy-image" + i).innerHTML = '<img src="' + value[i].imageUrl + '"alt=ours en peluche"></img>';
                 document.getElementById("teddy-name" + i).textContent = value[i].name;
                 document.getElementById("teddy-price" + i).textContent = value[i].price*0.01 + " €";
