0550ef3a8dc4db8b4289cfe8cb1cd372429a86c3
Toops61 <toops61@gmail.com>
2021-07-03 20:48:20 +0200

    update price quantite

--- BACK/panier.js
+++ BACK/panier.js
@@ -1,8 +1,8 @@
 let productsId = Object.getOwnPropertyNames(localStorage);
 let panierArray = [];
 let totalPanier = 0;
-let panierSection = document.getElementById('panier-section'); 
-panierSection.innerHTML += '<p id="total-price">total : </p>';
+let panierSection = document.getElementById('panier-section');
+
 for (let i = 0; i < productsId.length; i++) {
     fetch('http://localhost:3000/api/teddies/' + productsId[i])
         .then(function(res) {
@@ -24,46 +24,40 @@ for (let i = 0; i < productsId.length; i++) {
         .then(function(produits) {
             let produitPrice = produits[i].prix * 0.01;
             let panierSection = document.getElementById('panier-section');
-            panierSection.innerHTML += '<div class="panier-resume__article"><div class="panier-resume__article__image" tabindex="0" id="article-image' + i + '"></div><div class="panier-resume__article__details"><p tabindex="0" class="panier-resume__name">article : Ours en peluche ' + produits[i].name + '</p><p class="panier-resume__quantite">quantité : <input type="number" min="1" max="99" value="1" id="quantite-produit"></p><div tabindex="0" class="panier-resume__supprimer"><p>supprimer</p></div></div><p tabindex="0" class="panier-resume__price" id="produit-price">' + produitPrice + ' €</p></div>';
+            panierSection.innerHTML += '<div class="panier-resume__article"><div class="panier-resume__article__image" tabindex="0" id="article-image' + i + '"></div><div class="panier-resume__article__details"><p tabindex="0" class="panier-resume__name">article : Ours en peluche ' + produits[i].name + '</p><label for="quantite-produit' + i + '" class="panier-resume__quantite">quantité : </label><input type="number" min="1" max="99" value="1" id="quantite-produit' + i + '"><div tabindex="0" class="panier-resume__supprimer"><p>supprimer</p></div></div><p tabindex="0" class="panier-resume__price" id="produit-price' + i + '">' + produitPrice + ' €</p></div>';
             document.getElementById("article-image" + i).innerHTML = '<img src="' + produits[i].imageURL + '"alt=ours en peluche"></img>';
             totalPanier += produitPrice;
             return totalPanier;
-        })
-        .then(function(total) {     
-            document.getElementById('total-price').innerHTML = total + ' €';
         })
         .catch(function(error) {
             console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
         });
 }
 
+setTimeout(function() {
+    if (totalPanier === 0) {
+        panierSection.innerHTML = '<p>votre panier est vide</p>';
+    } else {
+        panierSection.innerHTML += '<p tabindex="0" id="total-price">total : ' + totalPanier + ' €</p>';
+    }
+}, 500);
 
-/* fetch('http://localhost:3000/api/teddies')
-    .then(function(res) {
-        if(res.ok) {
-            const panierArray = res.json();
-            return panierArray;
-        }
-    })
-    .then(function(value) {
-        let totalPanier = 0;
-        let articlesPanier = value;   
-        let panierSection = document.getElementById('panier-section');    
-        for (let i = 0; i < articlesPanier.length; i++) {
-            let produitPrice = value[i].price * 0.01;
-            panierSection.innerHTML += '<div class="panier-resume__article"><div class="panier-resume__article__image" id="article-image' + i + '"></div><div class="panier-resume__article__details"><p class="panier-resume__name">article : Ours en peluche ' + value[i].name + '</p><p class="panier-resume__quantite">quantité : <input type="number" min="1" max="99" value="1" id="quantite-produit"></p><div class="panier-resume__supprimer"><p>supprimer</p></div></div><p class="panier-resume__price" id="produit-price">' + produitPrice + ' €</p></div>';
-            document.getElementById("article-image" + i).innerHTML = '<img src="' + value[i].imageUrl + '"alt=ours en peluche"></img>';
-            let produitQuantite = document.getElementById('quantite-produit').value;
-            produitPrice = produitPrice * produitQuantite;
-            totalPanier += produitPrice;
-        }
-        panierSection.innerHTML += '<p id="total-price">total : </p>';
-        document.getElementById('total-price').innerHTML += totalPanier;
-    })
-    .catch(function(error) {
-        console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
-    });
+function updatePrice() {
+    totalPanier = 0;
+    for (let ind = 0; ind < panierArray.length; ind++) {
+        let quantite = document.getElementById('quantite-produit' + ind).value;
+        let prix = document.getElementById('produit-price' + ind);
+        let price = panierArray[ind].prix * .01;
+        totalPanier += price;
+        prix.textContent = price * quantite + ' €';
+        totalPanier += price * quantite - price;
+    };
+    document.getElementById('total-price').textContent = 'total : ' + totalPanier + ' €';
+}
 
-let submit = document.getElementById('submit-btn');
- */
-
+setTimeout(function() {
+    for (let index = 0; index < panierArray.length; index++) {
+        let input = document.querySelectorAll('section input')[index];
+        input.addEventListener('click', updatePrice);
+    }
+}, 2000);
